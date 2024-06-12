fdfdb7f7e5f71a29a33074fc304f696140da5332
Aman Kumar <99892898+aman-kumar29@users.noreply.github.com>
2024-06-12 09:11:30 +0000

    added profile page

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -9,29 +9,34 @@ import CheckoutPage from './pages/Checkout/CheckoutPage.js'
 import AuthRoute from './components/AuthRoute/AuthRoute.js'
 import PaymentPage from './pages/Payment/PaymentPage.js'
 import OrderTrackPage from './pages/OrderTrack/OrderTrackPage.js'
+import ProfilePage from './pages/Profile/ProfilePage.js'
 
 export default function AppRoutes() {
   return (
     <Routes>
-        <Route path = '/' element = {<HomePage/>}/>
-        <Route path="/search/:searchTerm" element={<HomePage />} />
-        <Route path="/tag/:tag" element={<HomePage />} />
-        <Route path="food/:id" element={<FoodPage/>} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path='/checkout' element={
+      <Route path='/' element={<HomePage />} />
+      <Route path="/search/:searchTerm" element={<HomePage />} />
+      <Route path="/tag/:tag" element={<HomePage />} />
+      <Route path="food/:id" element={<FoodPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path='/checkout' element={
         <AuthRoute>
-          <CheckoutPage/>
-        </AuthRoute>}/>
-        <Route path='/payment' element={
+          <CheckoutPage />
+        </AuthRoute>} />
+      <Route path='/payment' element={
         <AuthRoute>
-          <PaymentPage/>
-        </AuthRoute>}/>
-        <Route path='/track/:orderId' element={
+          <PaymentPage />
+        </AuthRoute>} />
+      <Route path='/track/:orderId' element={
         <AuthRoute>
-          <OrderTrackPage/>
-        </AuthRoute>}/>
+          <OrderTrackPage />
+        </AuthRoute>} />
+      <Route path='/profile' element={
+        <AuthRoute>
+          <ProfilePage />
+        </AuthRoute>} />
     </Routes>
   )
 }
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useState, createContext, useContext } from 'react';
-import * as userService from '../services/userService';
+import * as userService from '../services/userService.js';
 import { toast } from 'react-toastify';
 
 const AuthContext = createContext(null);
@@ -33,21 +33,21 @@ export const AuthProvider = ({ children }) => {
     toast.success('Logout Successful');
   };
 
-  // const updateProfile = async user => {
-  //   const updatedUser = await userService.updateProfile(user);
-  //   toast.success('Profile Update Was Successful');
-  //   if (updatedUser) setUser(updatedUser);
-  // };
+  const updateProfile = async user => {
+    const updatedUser = await userService.updateProfile(user);
+    toast.success('Profile Update Was Successful');
+    if (updatedUser) setUser(updatedUser);
+  };
 
-  // const changePassword = async passwords => {
-  //   await userService.changePassword(passwords);
-  //   logout();
-  //   toast.success('Password Changed Successfully, Please Login Again!');
-  // };
+  const changePassword = async passwords => {
+    await userService.changePassword(passwords);
+    logout();
+    toast.success('Password Changed Successfully, Please Login Again!');
+  };
 
   return (
     <AuthContext.Provider
-      value={{ user, login, logout, register,}}
+      value={{ user, login, logout, register, updateProfile, changePassword}}
     >
       {children}
     </AuthContext.Provider>
